25dc760967dd6d33504fe7a3b1f169f00d5f3c4b
Renaud Machecourt-Bourgeois <renaud.machecourt-bourgeois@etu.u-bordeaux.fr>
2022-03-02 15:14:55 +0100

    game_solve.c (related to #9)

---
+++ game_solve.c
@@ -0,0 +1,35 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "game.h"
+#include "game_aux.h"
+#include "game_ext.h"
+#include "game_tools.h"
+
+int main(int argc, char* argv[])
+{;
+    game current_game;
+    if (argc >= 3 && argc <= 4){
+        current_game = game_load(argv[2]);
+
+    } else{
+        return EXIT_FAILURE;
+    }
+    if (strcmp("-s", argv[1]) == 0){
+        game_solve(current_game);
+        if (argc == 4){
+            game_save(current_game, argv[3]);
+        } else{
+            game_print(current_game);
+        }
+    } else if (strcmp("-c", argv[1]) == 0){
+        if (argc == 4){
+            FILE *save = fopen(argv[3], "w");
+            fprintf(save, "%d", game_nb_solutions(current_game));
+            fprintf(save, "\n");
+        }
+    }
+    return EXIT_SUCCESS;
+}
