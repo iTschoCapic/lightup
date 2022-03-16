597461db8fb9705d240f803d4fab088e73716404
Maxime Audrain <maxime.audrain@etu.u-bordeaux.fr>
2022-03-16 15:23:13 +0100

    Process -> mouse button

--- model.c
+++ model.c
@@ -6,9 +6,10 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "game.h"
-#include "game_tools.h"
 #include "game_ext.h"
+#include "game_tools.h"
 
 /* **************************************************************** */
 
@@ -27,8 +28,8 @@ struct Env_t
     SDL_Texture *title;
     game jeu;
     SDL_Rect *cases;
-    int window_width , window_height ;
-    SDL_Point ligne_depart,ligne_arrivee;
+    int window_width, window_height;
+    SDL_Point ligne_depart, ligne_arrivee;
 };
 
 /* **************************************************************** */
@@ -38,9 +39,9 @@ Env *init(SDL_Window *win, SDL_Renderer *ren, int argc, char *argv[])
     Env *env = malloc(sizeof(struct Env_t));
     env->jeu = game_load(argv[2]);
     env->cases = 0;
-    SDL_GetWindowSize(win, &env->window_width , &env->window_height);
-    env->ligne_depart.x = env->window_width/(game_nb_rows(env->jeu)+2);
-    env->ligne_depart.y = env->window_height/(game_nb_cols(env->jeu)+2);
+    SDL_GetWindowSize(win, &env->window_width, &env->window_height);
+    env->ligne_depart.x = env->window_width / (game_nb_rows(env->jeu) + 2);
+    env->ligne_depart.y = env->window_height / (game_nb_cols(env->jeu) + 2);
     /* PUT YOUR CODE HERE TO INIT TEXTURES, ... */
 
     return env;
@@ -52,48 +53,48 @@ void render(SDL_Window *win, SDL_Renderer *ren, Env *env)
 { /* PUT YOUR CODE HERE TO RENDER TEXTURES, ... */
     env->cases[0].x = env->cases[0].y = 0;
     env->cases[0].w = env->cases[0].h = 100;
-    for(int i = 1; i != (game_nb_rows(env->jeu)*game_nb_cols(env->jeu)); i++)
+    for (int i = 1; i != (game_nb_rows(env->jeu) * game_nb_cols(env->jeu)); i++)
     {
-        env->cases[i].x = env->cases[i-1].x + 200;
-        env->cases[i].y = env->cases[i-1].y;
+        env->cases[i].x = env->cases[i - 1].x + 200;
+        env->cases[i].y = env->cases[i - 1].y;
 
-        if(i%7 == 0) //retour à la ligne
+        if (i % 7 == 0)  // retour à la ligne
         {
-            env->cases[i].x = (i%7 == 0) ? 0 : 100;
-            env->cases[i].y = env->cases[i-1].y + 100;
+            env->cases[i].x = (i % 7 == 0) ? 0 : 100;
+            env->cases[i].y = env->cases[i - 1].y + 100;
         }
-        env->cases[i].w = env->cases[i].h = 100; //taille d'une case : 100 x 100
+        env->cases[i].w = env->cases[i].h = 100;  // taille d'une case : 100 x 100
     }
-    if(SDL_RenderFillRects(ren,env->cases,(game_nb_rows(env->jeu)*game_nb_cols(env->jeu))) <0)//Remplissage des cases blanches
+    if (SDL_RenderFillRects(ren, env->cases, (game_nb_rows(env->jeu) * game_nb_cols(env->jeu))) < 0)  // Remplissage des cases blanches
     {
-        printf("Erreur lors des remplissages de rectangles: %s",SDL_GetError());
-        return ;
+        printf("Erreur lors des remplissages de rectangles: %s", SDL_GetError());
+        return;
     }
     // À présent, occupons nous des lignes
     // On ne peut pas utiliser la fonction SDL_RenderDrawLines
     // car celle-ci ne permet pas de créer des lignes indépendantes comme nous voulons le faire mais des chemins
 
-    SDL_SetRenderDrawColor(ren,0,0,0,255);//Couleur rouge
+    SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);  // Couleur rouge
 
     // Lignes horizontales
-    env->ligne_depart.x = env->window_width/(game_nb_rows(env->jeu)+2);
-    env->ligne_arrivee.x = (env->window_width/(game_nb_rows(env->jeu)+2))*(game_nb_cols(env->jeu)+1);
+    env->ligne_depart.x = env->window_width / (game_nb_rows(env->jeu) + 2);
+    env->ligne_arrivee.x = (env->window_width / (game_nb_rows(env->jeu) + 2)) * (game_nb_cols(env->jeu) + 1);
     env->ligne_depart.y = 0;
-    for(int i = 0; i!=(game_nb_cols(env->jeu)+1); i++)
+    for (int i = 0; i != (game_nb_cols(env->jeu) + 1); i++)
     {
-      env->ligne_depart.y += game_nb_cols(env->jeu);
-      env->ligne_arrivee.y = env->ligne_depart.y;
-      SDL_RenderDrawLine(ren,env->ligne_depart.x, env->ligne_depart.y,env->ligne_arrivee.x,env->ligne_arrivee.y);
+        env->ligne_depart.y += game_nb_cols(env->jeu);
+        env->ligne_arrivee.y = env->ligne_depart.y;
+        SDL_RenderDrawLine(ren, env->ligne_depart.x, env->ligne_depart.y, env->ligne_arrivee.x, env->ligne_arrivee.y);
     }
     // Lignes verticales
     env->ligne_depart.x = 0;
-    env->ligne_depart.y = env->window_width/(game_nb_rows(env->jeu)+2);
-    env->ligne_arrivee.y = (env->window_width/(game_nb_rows(env->jeu)+2))*(game_nb_cols(env->jeu)+1);
-    for(int i = 0; i!=(game_nb_rows(env->jeu)+1); i++)
+    env->ligne_depart.y = env->window_width / (game_nb_rows(env->jeu) + 2);
+    env->ligne_arrivee.y = (env->window_width / (game_nb_rows(env->jeu) + 2)) * (game_nb_cols(env->jeu) + 1);
+    for (int i = 0; i != (game_nb_rows(env->jeu) + 1); i++)
     {
-      env->ligne_depart.x += game_nb_rows(env->jeu);
-      env->ligne_arrivee.x = env->ligne_depart.x;
-      SDL_RenderDrawLine(ren,env->ligne_depart.x, env->ligne_depart.y,env->ligne_arrivee.x,env->ligne_arrivee.y);
+        env->ligne_depart.x += game_nb_rows(env->jeu);
+        env->ligne_arrivee.x = env->ligne_depart.x;
+        SDL_RenderDrawLine(ren, env->ligne_depart.x, env->ligne_depart.y, env->ligne_arrivee.x, env->ligne_arrivee.y);
     }
 }
 
@@ -101,15 +102,71 @@ void render(SDL_Window *win, SDL_Renderer *ren, Env *env)
 
 bool process(SDL_Window *win, SDL_Renderer *ren, Env *env, SDL_Event *e)
 {
+    int w, h;
+    SDL_GetWindowSize(win, &w, &h);
+
+    /* generic events */
     if (e->type == SDL_QUIT)
     {
         return true;
     }
+    /* Android events */
+#ifdef __ANDROID__
+    else if (e->type == SDL_FINGERDOWN)
+    {
+        env->mario_x = e->tfinger.x * w; /* tfinger.x, normalized in [0..1] */
+        env->mario_y = e->tfinger.y * h; /* tfinger.y, normalized in [0..1] */
+    }
+    /* other events */
+#else
+    else if (e->type == SDL_MOUSEBUTTONDOWN)
+    {
+        SDL_Point mouse;
+        Uint32 button = SDL_GetMouseState(&mouse.x, &mouse.y);
+        int case_i = 0;
+        int case_j = 0;
+        if ((e->button.button) == 1)
+        {
+            if (game_check_move(env->jeu, case_i, case_j, S_LIGHTBULB))
+            {
+                game_play_move(env->jeu, case_i, case_j, S_LIGHTBULB);
+            }
+        }
+        else if ((e->button.button) == 1)
+        {
+            if (game_check_move(env->jeu, case_i, case_j, S_MARK))
+            {
+                game_play_move(env->jeu, case_i, case_j, S_MARK);
+            }
+        }
+    }
+    else if (e->type == SDL_KEYDOWN)
+    {
+        /*switch (e->key.keysym.sym)
+        {
+            case SDLK_LEFT:
+                env->bomb_x -= 10;
+                break;
+            case SDLK_RIGHT:
+                env->bomb_x += 10;
+                break;
+            case SDLK_UP:
+                env->bomb_y -= 10;
+                break;
+            case SDLK_DOWN:
+                env->bomb_y += 10;
+                break;
+            case SDLK_ESCAPE:
+                return true;
+                break;
+        }*/
+    }
+#endif
 
-    /* PUT YOUR CODE HERE TO PROCESS EVENTS */
-
-    return false;
+    return false; /* don't quit */
 }
+
+/* PUT YOUR CODE HERE TO PROCESS EVENTS */
 
 /* **************************************************************** */
 
