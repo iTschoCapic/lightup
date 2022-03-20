f38423116b25c286c122bdb24b1781e4bdc36414
Loïc <loic.richard@etu.u-bordeaux.fr>
2022-03-20 16:54:46 +0100

    redimension grille tjrs carre masi pas tjrs centre (related to #10)

--- model.c
+++ model.c
@@ -73,27 +73,34 @@ void render(SDL_Window *win, SDL_Renderer *ren, Env *env)
     // car celle-ci ne permet pas de créer des lignes indépendantes comme nous voulons le faire mais des chemins
 
     SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);  // Couleur rouge
-
+    int size = 0;
+    if(env->window_height < env->window_width){
+        size = env->window_height;
+    }
+    else
+    {
+        size = env->window_width;
+    }
     // Lignes horizontales
-    env->ligne_depart.x = env->window_width / (game_nb_cols(env->jeu) + 2);
-    env->ligne_arrivee.x = (env->window_width / (game_nb_cols(env->jeu) + 2)) * (game_nb_cols(env->jeu) + 1);
-    env->ligne_depart.y = env->window_height/(game_nb_rows(env->jeu)+2);
-    env->ligne_arrivee.y = env->window_height/(game_nb_rows(env->jeu)+2);
+    env->ligne_depart.x = size / (game_nb_cols(env->jeu) + 2);
+    env->ligne_arrivee.x = (size / (game_nb_cols(env->jeu) + 2)) * (game_nb_cols(env->jeu) + 1);
+    env->ligne_depart.y = size/(game_nb_rows(env->jeu)+2);
+    env->ligne_arrivee.y = size/(game_nb_rows(env->jeu)+2);
     for (int i = 0; i != (game_nb_rows(env->jeu) + 1); i++)
     {
         SDL_RenderDrawLine(ren,env->ligne_depart.x, env->ligne_depart.y,env->ligne_arrivee.x,env->ligne_arrivee.y);
-        env->ligne_depart.y += env->window_height/(game_nb_rows(env->jeu)+2);
+        env->ligne_depart.y += size/(game_nb_rows(env->jeu)+2);
         env->ligne_arrivee.y = env->ligne_depart.y;
     }
     // Lignes verticales
-    env->ligne_depart.x = env->window_width/(game_nb_cols(env->jeu)+2); 
-    env->ligne_arrivee.x = env->window_width/(game_nb_cols(env->jeu)+2);
-    env->ligne_depart.y = env->window_height/(game_nb_rows(env->jeu)+2);
-    env->ligne_arrivee.y = (env->window_height / (game_nb_rows(env->jeu) + 2)) * (game_nb_rows(env->jeu) + 1);
+    env->ligne_depart.x = size/(game_nb_cols(env->jeu)+2); 
+    env->ligne_arrivee.x = size/(game_nb_cols(env->jeu)+2);
+    env->ligne_depart.y = size/(game_nb_rows(env->jeu)+2);
+    env->ligne_arrivee.y = (size / (game_nb_rows(env->jeu) + 2)) * (game_nb_rows(env->jeu) + 1);
     for(int i = 0; i!=(game_nb_cols(env->jeu)+1); i++)
     {
         SDL_RenderDrawLine(ren,env->ligne_depart.x, env->ligne_depart.y,env->ligne_arrivee.x,env->ligne_arrivee.y);
-        env->ligne_depart.x += env->window_width/(game_nb_cols(env->jeu)+2);
+        env->ligne_depart.x += size/(game_nb_cols(env->jeu)+2);
         env->ligne_arrivee.x = env->ligne_depart.x;
         
     }
